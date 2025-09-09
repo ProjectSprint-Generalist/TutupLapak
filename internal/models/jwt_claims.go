4e86ccadf49daff963338a7af49a61833f908b5d
Nurfadillah Umar <itsennuu@gmail.com>
2025-09-09 20:18:34 +0800

    fix: make email nullable in JWTClaim payload

--- internal/models/jwt_claims.go
+++ internal/models/jwt_claims.go
@@ -5,6 +5,6 @@ import "github.com/golang-jwt/jwt/v5"
 // JWT Payload
 type JWTClaim struct {
 	ID    uint   `json:"id"`
-	Email string `json:"email"`
+	Email *string `json:"email"`
 	jwt.RegisteredClaims
 }
